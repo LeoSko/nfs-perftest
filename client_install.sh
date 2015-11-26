9a96b53036eec73ae3149168ec8cafb2aa2e9a83
Leonid Skorospelov <leosko94@mail.ru>
2015-11-26 18:53:45 +0300

    Fixed client, profiler raw update

--- client_install.sh
+++ client_install.sh
@@ -1,4 +1,14 @@
-mkdir /home/$(whoami)/NFS
+#!/bin/bash
+if test "$#" -ne 1; then
+	echo "There should be a hostname of NFS server or it's ip as a parameter"
+	exit 1
+fi
+uname=$(whoami)
+remote_ip=$1
+remote_folder=$remote_ip:/home/$uname/Share
+local_folder=/home/$uname/NFS
+echo "Connecting to $remote_ip"
+mkdir $local_folder
 sudo apt-get update
 sudo apt-get -qq install nfs-kernel-server nfs-common
-sudo mount -t nfs -O uid=1000,iocharset=utf-8 $1:/home/$(whoami)/Share /home/$(whoami)/NFS
+sudo mount -t nfs -O uid=1000,iocharset=utf-8 $remote_folder $local_folder
--- profileme.sh
+++ profileme.sh
@@ -2,13 +2,23 @@
 sudo apt-get install -qq iozone3
 uname=$(whoami)
 sharedir=$(df /home/$uname/NFS | cut -d " " -f 1 | tail -1)
-mkdir /home/$uname/log
+if [ -z "$sharedir" ]; then
+	echo "You have to mount NFS folder into /home/$uname/NFS to run this script"
+	exit 1
+fi
+logdir=/home/$uname/log
+local_dir=/home/$uname/NFS
+test_file=$local_dir/test.img
+mkdir $logdir
+
+sudo cp /etc/fstab /home/$uname/fstab
 for rsize in 1024 2048 4096 8192
 do
-	sudo cp /etc/fstab /home/$uname/fstab
-	sudo echo "$sharedir /home/$uname/NFS nfs rw,user,noauto,rsize=$rsize 0 0" >> /home/$uname/fstab
 	sudo cp /home/$uname/fstab /etc/fstab
-	sudo umount /home/$uname/NFS
-	sudo mount -t nfs -O uid=1000,iosharset=utf-8,rsize=$rsize $sharedir /home/$uname/NFS
-	sudo iozone -a -R -c -U /home/$uname/NFS -f /home/$uname/NFS/test.img > /home/$uname/log/rs$rsize.log
+	echo "$sharedir $local_dir nfs rw,user,noauto,rsize=$rsize 0 0" | sudo tee --append /etc/fstab
+#	sudo umount $local_dir
+#	sudo mount -t nfs -O uid=1000,iosharset=utf-8,rsize=$rsize $sharedir $local_dir
+	sudo iozone -a -R -c $local_dir -f $test_file > $logdir/rs$rsize.log
 done
+sudo cp /home/$uname/fstab /etc/fstab
+
