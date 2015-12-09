1bd3c5b470de6ce57ad807150e4c81a080798897
Leonid <leosko94@mail.ru>
2015-12-09 23:46:41 +0300

    Update server_install.sh

--- server_install.sh
+++ server_install.sh
@@ -2,7 +2,7 @@
 sudo apt-get update
 sudo apt-get install -qq nfs-kernel-server nfs-common 
 mkdir /home/$(whoami)/Share
-echo -e "/home/$(whoami)/Share $(hostname -I | xargs)\b/255.255.255.0(rw,insecure,nohide,no_root_squash,no_subtree_check,sync)" | grep ' ' | sudo tee --append /etc/exports > /dev/null
+echo -e "/home/$(whoami)/Share *(rw,insecure,nohide,no_root_squash,no_subtree_check,sync)" | grep ' ' | sudo tee --append /etc/exports > /dev/null
 sudo exportfs -a
 sudo /etc/init.d/nfs-kernel-server restart
 echo "Test" > /home/$(whoami)/Share/Testfile
